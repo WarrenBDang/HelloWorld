7053d35dab77d034595ff6a6d7851e88f4295b23
bdang <bdaang@protonmail.ch>
2022-03-22 19:02:19 -0500

    Initial Commit

---
+++ HelloWorld.py
@@ -0,0 +1,51 @@
+Programming 101
+
+Integrated Development Environmen (IDE)
+
+
+
+def dataTypes():
+    # Exercise 1: String - string of chars
+    stringA = 'Hello'
+    stringB = 'World'
+    print(stringA + stringB)
+
+    # stringA = 'World'
+    # stringB = 'Hello'
+    # print(stringA + stringB)
+
+    # # Exercise 2: Numbers
+
+    # # Integers - whole number values
+    # int = 1
+    # print(int)
+    # # print(str(int))
+
+    # # Float - floating point values (decimals)
+    # float = 1.9
+    # print(float)
+    # # print(str(float))
+
+    # # Exercise 3: Boolean - True or False values
+    # stringA = 'This'
+    # stringB = 'value'
+    # stringC = 'is'
+    # bool = True
+    # print(stringA + stringB + stringC + bool)
+
+    # # Exercise 4 - Array/List - a list of items usually of the same datatype, but can hold different data types.
+    # array = []
+    # print(array)
+
+    # # Exercise 5 - Dictionary - a map of key-to-value pair.
+    # dict = {"key": 'value'}
+    # print(dict)
+    return
+
+def main(): # Data-types
+    dataTypes()
+    return
+
+#################################################################
+main()
+
