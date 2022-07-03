e602faf612c3799ac2b97e6618f33060b5c5ee76
bdang <bdaang@protonmail.ch>
2022-07-03 17:19:45 -0500

    Added while loops

--- DecisionStructures.py
+++ DecisionStructures.py
@@ -1,16 +1,25 @@
-
-
 def ifStatement():
+    answer = input("Type y or n ")
+    if answer == "y":
+        print("Your answer is YES")
+    elif answer == "n":
+        print("Your answer is NO")
+    else:
+        print("Answer is not valid")
     return
 
 def whileStatement():
+    answer = int(input("Type a number greater an 0 "))
+    while answer != 0:
+        print(answer)
+        answer = answer - 1
     return
 
 def forStatement():
     return
 
 def main():
-    ifStatement()
+    # ifStatement()
     whileStatement()
     forStatement()
     return
