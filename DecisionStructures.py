250dfa6c69761b092cca3bdf68629364dddbffbc
bdang <bdaang@protonmail.ch>
2022-07-03 17:07:59 -0500

    Initial Commit

---
+++ DecisionStructures.py
@@ -0,0 +1,21 @@
+
+
+def ifStatement():
+    return
+
+def whileStatement():
+    return
+
+def forStatement():
+    return
+
+def main():
+    ifStatement()
+    whileStatement()
+    forStatement()
+    return
+
+
+
+######################
+main()
