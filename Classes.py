18806d8dd6ad725f4f8442541b58e07ecb7badc5
bdang <bdaang@protonmail.ch>
2022-07-03 20:08:05 -0500

    Initial Commit

---
+++ Classes.py
@@ -0,0 +1,11 @@
+# Classes and Object-Oriented Programming
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
